0713828d1e454ff44d7727d08ca32d45849e8fb3
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-31 19:27:31 -0400

    system for checking in the eventuser is broken

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -4,8 +4,11 @@ class EventUsersController < ApplicationController
   def create
     @event = Event.find(params[:event_id].to_i)
     @event_user = EventUser.new(user_id: current_user.id, event_id: @event.id)
-
-    if @event_user.save
+    byebug
+    #fix needed
+    if EventUser.where(user_id: current_user, event_id: @event.id)
+      flash[:error] = "You cannot attend the same event twice"
+    elsif @event_user.save
       flash[:notice] = "Looking forward to seeing you there!"
     else
       flash[:error] = "Something went wrong"
