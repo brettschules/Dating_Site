e1254ab2db48ceee816571c21d35bcc095ff9760
Brett Schules <brettschules@gmail.com>
2017-06-01 10:45:57 -0400

    EventUser validation

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -3,17 +3,21 @@ class EventUsersController < ApplicationController
 
   def create
     @event = Event.find(params[:event_id].to_i)
-    @event_user = EventUser.new(user_id: current_user.id, event_id: @event.id)
-    byebug
+    @user = current_user
+    @event_user = EventUser.new(user_id: @user.id, event_id: @event.id)
+
     #fix needed
-    if EventUser.where(user_id: current_user, event_id: @event.id)
-      flash[:error] = "You cannot attend the same event twice"
-    elsif @event_user.save
+    #if @event_users.user_id == current_user.id
+    # EventUser.where(user_id: current_user, event_id: @event.id)
+    # this is not working bc it's has to be saved in DB first?
+
+    if @event_user.save
       flash[:notice] = "Looking forward to seeing you there!"
     else
-      flash[:error] = "Something went wrong"
+      flash[:error] = "You cannot attend the same event twice"
     end
     redirect_to event_path(@event)
+
   end
 
 
--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,4 +1,14 @@
 class EventUser < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  validate :exists?
+
+  def exists?
+
+    # if EventUser.where(user_id: self.user.id, event_id: self.event.id) || EventUser.where(user_id: self.user.id, event_id: self.event.id).any?
+    if self.event.host_id == self.event.host_id && EventUser.where(user_id: self.user.id, event_id: self.event.id).any?
+      errors.add(:base, "You cannot attend the same event twice")
+    end
+  end
 end
